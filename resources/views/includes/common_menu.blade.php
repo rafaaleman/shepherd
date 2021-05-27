2893c0244b1c47d5da6b474c736a6428b58d8f00
Rafa Alemán <raleman@holadoctor.net>
2021-05-26 20:06:55 -0500

    Get notifications & notifications on menu

--- resources/views/includes/common_menu.blade.php
+++ resources/views/includes/common_menu.blade.php
@@ -1,18 +1,18 @@
 <a class="menu-link item d-block d-md-none" href="{{ route('home') }}">
     <i class="fas fa-users fa-2x"></i> <span>Dashboard</span>
 </a>
-<a class="menu-link item d-block active" href="{{ route('careteam', '**SLUG**')}}">
+<a class="menu-link item d-block {{(Request::is('careteam/*') ) ? 'active' : ''}}" href="{{ route('careteam', '**SLUG**')}}">
     <i class="fas fa-users fa-2x"></i> <span>CareTeam</span>
 </a>
-<a class="menu-link item d-block" href="{{ route('carehub', '**SLUG**')}}">
+<a class="menu-link item d-block {{(Request::is('carehub/*') ) ? 'active' : ''}} " href="{{ route('carehub', '**SLUG**')}}">
     <i class="far fa-calendar-plus  fa-2x"></i> <span>CareHub</span>
 </a>
-<a class="menu-link item d-block" href="{{ route('lockbox', '**SLUG**')}}">
+<a class="menu-link item d-block {{(Request::is('lockbox/*') ) ? 'active' : ''}}" href="{{ route('lockbox', '**SLUG**')}}">
     <i class="fas fa-file-medical fa-2x"></i> <span>LockBox</span>
 </a>
-<a class="menu-link item d-block" href="">
+<a class="menu-link item d-block {{(Request::is('medlist/*') ) ? 'active' : ''}}" href="">
     <i class="fas fa-prescription-bottle-alt fa-2x"></i> <span>MedList</span>
 </a>
-<a class="menu-link item d-block" href="">
+<a class="menu-link item d-block {{(Request::is('resources/*') ) ? 'active' : ''}}" href="">
     <i class="fas fa-globe"></i> <span>Resources</span>
 </a>
