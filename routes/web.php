d7b51574aa79362501b8c0966db83d97bf58dbf5
rromero2991 <rafael.alexromero@gmail.com>
2021-07-01 22:53:03 -0500

    add company to user after to revert

--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,7 @@ Auth::routes([
 Route::post('/user/profile/update', 'Auth\RegisterController@updateUser')->name('user.profile.update')->middleware('auth');
 Route::get('/user/profile', 'HomeController@profile')->name('user.profile')->middleware('auth');
 
-Route::get('/register/{token}', 'Auth\RegisterController@showRegistrationForm2')->name('register_invitation');
+Route::get('/register/{token}/{company?}', 'Auth\RegisterController@showRegistrationForm2')->name('register_invitation');
 Route::get('/home', 'HomeController@index')->name('home');
 
 Route::get('/new', 'HomeController@newUser')->name('new');
@@ -23,4 +23,5 @@ Route::get('/new', 'HomeController@newUser')->name('new');
 Route::get("/resources/{loveone_slug}","ResourceController@getTopics")->name("resources");
 Route::post("/resources/search","ResourceController@getTopicsSearch")->name("resources.search");
 Route::post("/resources/search/ini","ResourceController@getTopicsSearchIni")->name("resources.ini");
+
 
