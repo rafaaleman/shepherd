498f1540a8d43f3f197e4dab437d37899e9e89e8
Rafa Alemán <raleman@holadoctor.net>
2021-07-19 08:40:28 -0500

    fix url variable

--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,7 @@ Auth::routes([
 Route::post('/user/profile/update', 'Auth\RegisterController@updateUser')->name('user.profile.update')->middleware('auth');
 Route::get('/user/profile', 'HomeController@profile')->name('user.profile')->middleware('auth');
 
-Route::get('/register/{token}/{company?}', 'Auth\RegisterController@showRegistrationForm2')->name('register_invitation');
+Route::get('/register/{token}', 'Auth\RegisterController@showRegistrationForm2')->name('register_invitation');
 Route::get('/home', 'HomeController@index')->name('home');
 
 Route::get('/new', 'HomeController@newUser')->name('new');
