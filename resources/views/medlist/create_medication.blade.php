c13c407056ae5936ef1570c7da4ca12d97218134
Rafael Romero <rromero@holadoctor.net>
2021-08-07 18:44:09 -0500

    Medlist, add frecuency, bug validation to required fields

--- resources/views/medlist/create_medication.blade.php
+++ resources/views/medlist/create_medication.blade.php
@@ -245,7 +245,7 @@
                 <div class="col-12 col-sm-12 col-lg-6 col-xl-6 my-2">
                     <div class="card shadow-sm my-2 col-12">
                         <div class="card-body">
-                        <input id="time" type="time" class="form-control no-border" name="time" value="" required autocomplete="time" onclick="" v-model="medication.time" placeholder="Time" min="{{ $date_now->format('g:i a') }}">
+                        <input id="time" type="text" class="form-control no-border" name="time" value="" required v-model="medication.time" placeholder="Start Time" min="{{ $date_now->format('g:i a') }}">
                         </div>
                     </div>
                 </div>
@@ -376,7 +376,8 @@
 @endsection
 
 @push('styles')
-
+<link href="{{asset('public/css/iconos_datepicker.css')}}" rel="stylesheet">
+<link href="{{asset('public/css/bootstrap-datetimepicker.css')}}" rel="stylesheet">
 <style>
 /* The customcheck */
 .ccheck{
@@ -410,6 +411,8 @@
     width: 25px;
     background-color: #fff;
     border-radius: 25px;
+    border: 1px solid;
+
 }
 
 /* On mouse-over, add a grey background color */
@@ -421,6 +424,7 @@
 .customcheck input:checked ~ .checkmark {
     background-color: #369bb6;
     border-radius: 25px;
+    border:0;
 }
 
 /* Create the checkmark/indicator (hidden when not checked) */
@@ -488,14 +492,26 @@
 .dropdown-item em{
     color:#235c6b !important;
 }
+#create_medication .dropdown-toggle{
+    color: #495057 !important;
+}
 </style>
 @endpush
 
 @push('scripts')
+<script src="{{asset('public/js/datetimepicker/bootstrap-datetimepicker.min.js')}}"></script>
 
 
 <script>
-    const create_event = new Vue({
+
+$(function () {
+    $('#time').datetimepicker({format: 'LT'});
+    $('#time').on('dp.change',function(e){
+        create_medication.medication.time= moment(e.date,"h:mm:ss a").format('h:mm a');
+    });
+});
+
+    const create_medication = new Vue({
         el: '#create_medication',
         created: function() {
             console.log('create_medication');
@@ -566,8 +582,7 @@
                             const resp = await axios.post(url, {keyword:medicineSelected.drugbank_pcid});
                             console.log(resp.data.routes);
                             this.routes = resp.data.routes;
-                            //this.medications_search = resp.data.medicines;
-                            //$("#message-search").removeClass('d-none').html('Results');
+                            
                         } catch (err) {
                             // Handle Error Here
                             console.error(err);
@@ -601,12 +616,12 @@
                 
 
                 //console.log(this.current_slug);
-                $('.loadingBtn').html('<span class="spinner-border spinner-border-sm mr-2" role="status" aria-hidden="true"></span>' + $('.loadingBtn').data('loading-text')).attr('disabled', true);
-                //this.event.phone = this.event.phone.replace(/\D/g,'');
                 
                 
-                   // console.log(url_succes);
-                //if(this.event.assigned.length > 0){
+                if(this.medication.route != "" && this.medication.dosage != ""  && this.medication.frequency != ""  && this.medication.time != ""  && this.medication.days != ""){
+                    $('.loadingBtn').html('<span class="spinner-border spinner-border-sm mr-2" role="status" aria-hidden="true"></span>' + $('.loadingBtn').data('loading-text')).attr('disabled', true);
+                    //this.event.phone = this.event.phone.replace(/\D/g,'');
+                    this.medication.time= moment(this.medication.time,"h:mm:ss a").format('HH:mm');
                     var url = '{{ route("medlist.create") }}';
                     
                     axios.post(url, this.medication).then(response => {
@@ -646,13 +661,27 @@
 
                         swal('There was an Error', txt, 'error');
                     });
-                /*}else{
-                    msg = 'has not assigned the sale to anyone. Please try again';
-                    icon = 'error';
-                    $("#collapseMemers").addClass("show");
-                    $('.loadingBtn').html('Save').attr('disabled', false);
-                    swal(msg, "", icon);
-                }*/
+                }else{
+                    
+                    if(this.medication.route == "" ){
+                        msg = 'Please enter "Route"';
+                        $("#collapseRoute").addClass("show");
+                    }else if(this.medication.dosage == ""){
+                        msg = 'Please enter "Dosage"';
+                        $("#collapseDosage").addClass("show");
+                    }else if(this.medication.frequency == ""){
+                        msg = 'Please enter "Frequency"';
+                        $("#collapsefrequency").addClass("show");
+                    }else if(this.medication.time == ""){
+                        msg = 'Please enter "Start time"';
+                    }else if(this.medication.days == ""){
+                        msg = 'Please enter "Days"';
+                        $("#collapseDays").addClass("show");
+                    }
+                        icon = 'error';
+                    // $('.loadingBtn').html('Save').attr('disabled', false);
+                        swal(msg, "", icon);
+                }
             },
             toFormData: function(obj){
                 var form_data = new FormData();
--- resources/views/medlist/medlist_modal.blade.php
+++ resources/views/medlist/medlist_modal.blade.php
@@ -1,31 +1,53 @@
-<div class="modal fade" tabindex="-1" role="dialog" id="medlist-modal">
+<div class="modal fade" tabindex="-1" role="dialog" id="api-medlist-modal">
     <div class="modal-dialog modal-dialog-centered " role="document">
         <div class="modal-content">
-        <div class="modal-header">
-            <h5 class="modal-title"><span>MEDLIST</span></h5>
-            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                <span aria-hidden="true">&times;</span>
-            </button>
-        </div>
-        <div class="modal-body ">
-           <h3><b>@{{products.name}}</b><br> <small>@{{products.drugbank_pcid}}</small></h3>
+            <div class="modal-header">
+                <h5 class="modal-title"><span>MEDLIST</span></h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body ">
+                <h3><b>@{{products.name}}</b><br> <small>@{{products.drugbank_pcid}}</small></h3>
 
-                <a href="#" class="btn btn-link" >Side Effects</a><br>
+                <a href="#" class="btn btn-link">Side Effects</a><br>
                 <a href="#" class="btn btn-link">Pregnancy / Breastfeeding</a><br>
                 <a href="#" class="btn btn-link">Drug Interactions</a><br>
                 <a href="#" class="btn btn-link">Dosage</a><br>
                 <a href="#" class="btn btn-link">A disabled link item</a>
-            <!--div class="list-group">
-                <template v-for="med in medication_complete">
-                    <div class="d-flex w-100 justify-content-between active">
-                        <h5 class="mb-1"><b>@{{med.date_usa}}</b> @{{med.medicine}}</h5>
-                        <small><b>@{{med.time}}</b></small>
-                    </div>
-                </template>
-            </div-->
-           <h4>@{{products.name}} Prescribing Information</h4>
-           <p class="text-justify">is an ultra short-acting benzodiazepine used in the induction and maintenance of sedation during short (<30 minute) procedures.6 Recent trends in anesthesia-related drug development have touted the benefits of so-called "soft drugs" - these agents, such as remifentanil, are designed to be metabolically fragile and thus susceptible to rapid biotransformation and elimination as inactive metabolites.4 These "soft drugs" are useful in the context of surgical procedures, wherein a rapid onset/offset is desirable, enabling anesthesiologists to manipulate drug concentrations as needed.3,4 Remimazolam was the first "soft" benzodiazepine analog to be developed5 and was approved for use by the FDA in July 2020 under the brand name Byfavo.</p>
+
+                <h4>@{{products.name}} Prescribing Information</h4>
+                <p class="text-justify">is an ultra short-acting benzodiazepine used in the induction and maintenance of sedation during short (<30 minute) procedures.6 Recent trends in anesthesia-related drug development have touted the benefits of so-called "soft drugs" - these agents, such as remifentanil, are designed to be metabolically fragile and thus susceptible to rapid biotransformation and elimination as inactive metabolites.4 These "soft drugs" are useful in the context of surgical procedures, wherein a rapid onset/offset is desirable, enabling anesthesiologists to manipulate drug concentrations as needed.3,4 Remimazolam was the first "soft" benzodiazepine analog to be developed5 and was approved for use by the FDA in July 2020 under the brand name Byfavo.</p>
+            </div>
         </div>
+    </div>
+</div>
+
+<div class="modal fade" tabindex="-1" role="dialog" id="medlist-modal">
+    <div class="modal-dialog modal-dialog-centered " role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title"><span>MEDLIST</span></h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body ">
+                <h3><b>@{{medication_view.medicine}}</b><br> <small>@{{products.drugbank_pcid}}</small></h3>
+
+                <ul class="list-group list-group-flush" v-for="med in medication_complete">
+                    <li class="list-group-item">
+                        <h5 class="mb-1"><b>@{{med.time_cad_gi}} @{{med.time_cad_a}}</b> <small>@{{med.date_usa}}</small>
+                       {{-- <button type="button" class="close" @click="deleteMedication(med)" >
+                            <span aria-hidden="true">&times;</span>
+                        </button></h5> --}}
+                    </li>
+                </ul>
+
+
+
+
+            </div>
         </div>
     </div>
 </div>
@@ -37,8 +59,8 @@
 
 @push('scripts')
 <script>
-$(function(){
-    
-});
+    $(function() {
+
+    });
 </script>
 @endpush
