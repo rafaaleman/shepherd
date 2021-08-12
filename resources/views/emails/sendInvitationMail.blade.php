0598d887482b2104923ad74b26b8cdd1306efc03
Etni Estrella <eestrella@holadoctor.net>
2021-08-12 00:01:39 -0400

    Fix invite email text

--- resources/views/emails/sendInvitationMail.blade.php
+++ resources/views/emails/sendInvitationMail.blade.php
@@ -5,12 +5,14 @@
 </a>
 
 <br><br>
-Hey!
-
 @component('mail::table')
 <img src="{{ config('app.url') . $details['loveone_photo']}}" alt="Shepherd" title="Shepherd" style="float: left; margin-right: 10px; border-radius: 50%; width: 150px;"> 
-You have been chosen to become part of the <i>CareTeam</i> of <strong>{{$details['loveone_name']}}</strong> in Shepherd Platform. <br><br>
-If you agree, please click on the button below to register into Shepherd. 
+You have been invited to become a member of the CareTeam for {{$details['loveone_name']}} in the ShepherdCares platform.
+
+ShepherdCares enables families to more easily manage the care and affairs of their loved ones who need assistance. You would be a valuable addition to this team.
+
+If you would like to join this CareTeam, please click on the button below to register.
+
 @endcomponent
 
 <br><br>
