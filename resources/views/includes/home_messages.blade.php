bf573ea0f7f062a9c4089adb86848f448d741651
Etni Estrella <eestrella@holadoctor.net>
2021-10-12 12:56:29 -0400

    add dashboard styling changes

--- resources/views/includes/home_messages.blade.php
+++ resources/views/includes/home_messages.blade.php
@@ -1,11 +1,11 @@
-<a :href="messages_url" class="widget team shadow-sm">
+<a :href="messages_url" class="card widget messages shadow-sm">
     <div class="card h-100">
         <div class="card-body">
             
             <h5 class="card-title"><i class="fas fa-comments fa-2x"></i> Messages</h5>
-            {{-- <p class="card-text">
-                <span>4</span> New Message(s) <br>    
-            </p> --}}
+            <p class="card-text">
+                <span>0</span> New Message(s) <br>    
+            </p>
         </div>
     </div>
 </a>
