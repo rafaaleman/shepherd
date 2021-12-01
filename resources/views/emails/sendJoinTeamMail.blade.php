251fc76e642aa31be9040fde80d1b7a7b8f57018
Etni Estrella <eestrella@holadoctor.net>
2021-12-01 09:12:25 -0500

    change text in emails

--- resources/views/emails/sendJoinTeamMail.blade.php
+++ resources/views/emails/sendJoinTeamMail.blade.php
@@ -10,7 +10,7 @@
     <img src="{{ config('app.url') . $details['loveone_photo']}}" alt="Shepherd" title="Shepherd" style="float: left; margin-right: 10px; border-radius: 50%; width: 150px;">  
 @endif
 {{$details['leader']}} has invited to become a member of the CareTeam for {{$details['loveone_name']}} in the ShepherdCares platform.
-<br/>
+<br/><br/>
 ShepherdCares enables families to more easily manage the care and affairs of their loved ones who need assistance. You would be a valuable addition to this team.
 
 If you would like to join this CareTeam, please click on the button below to register.
@@ -21,7 +21,7 @@ If you would like to join this CareTeam, please click on the button below to reg
 
 <br><br>
 @component('mail::button', ['url' => $details['url'], 'color' => 'primary'])
-Register
+Join Care Team
 @endcomponent
 
 <br>
