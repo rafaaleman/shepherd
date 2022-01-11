d1a8ea0aeab80ae11576136ea0e212329c8bb1e1
Rafael Romero <rromero@holadoctor.net>
2022-01-11 09:53:17 -0600

    fix, two factor disabled

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -42,7 +42,7 @@ class LoginController extends Controller
 
     protected function authenticated(Request $request, $user)
     {
-        $user->generateTwoFactorCode();
-        $user->notify(new TwoFactorCode());
+       // $user->generateTwoFactorCode();
+       // $user->notify(new TwoFactorCode());
     }
 }
